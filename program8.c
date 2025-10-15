3e21fccb307ccf049de0ff282a37c30dc31ec285
sakshi kachate <sakshikachate347@gmail.com>
2025-10-15 23:15:14 +0530

    Add files via upload

---
+++ program8.c
@@ -0,0 +1,31 @@
+
+
+
+#include<stdio.h>
+
+int AddionTwoNumbers(float fNo1,float fNo2)
+{
+  float fSum =0;
+  fSum = fNo1+fNo2;     ///Business logic
+  return fSum;
+}
+
+
+
+int main()
+{
+    float fValue1 = 0.0f, fValue2 = 0.0, fRet = 0.0;
+
+    printf("enter first no: \n");
+    scanf("%f",&fValue1);
+
+    printf("enter first no: \n");
+    scanf("%f",&fValue2);
+    
+    fRet = AddionTwoNumbers(fValue1,fValue2);
+
+    printf(" Addition is %f\n",fRet);
+
+
+    return 0;
+}
