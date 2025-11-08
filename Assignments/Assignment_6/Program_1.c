e97d7e35a8c1b63e3fe2d7958dea03d3fdd98437
sakshi kachate <sakshikachate347@gmail.com>
2025-11-08 15:03:43 +0530

    Accept Name from user and  Display it

---
+++ Assignments/Assignment_6/Program_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char Name[30];
+
+    printf("Please enter your full Name:");
+    scanf("%s",&Name);
+
+    printf("Your name %s", Name);
+
+    return 0;
+}
