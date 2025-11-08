c2b63bbb4511c7a830fc8a3506f37e4518efe267
sakshi kachate <sakshikachate347@gmail.com>
2025-11-08 14:33:16 +0530

    display multiplication of  number of Factors

---
+++ Assignments/Assignment_4/Program_1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    long mul = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+    for (i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            printf("%d ", i);
+            mul = mul * i;
+        }
+    }
+
+    printf("\nMultiplication of factors = %ld\n", mul);
+
+    return 0;
+}
