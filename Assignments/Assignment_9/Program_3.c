f113eb75bed5d1c9b5091ef1574b9d3edfe7fddb
sakshi kachate <sakshikachate347@gmail.com>
2025-11-09 12:11:24 +0530

    display even factorial of number

--- Assignments/Assignment_9/Program_3.c
+++ Assignments/Assignment_9/Program_3.c
@@ -1,27 +1,36 @@
 #include<stdio.h>
 
-int DollerToINT(int iNo)
-{
-    float frate = 0;
 
-    frate = 70;
-    return  iNo * frate;  
+
+int EvenFactorial(int iNo) {
+    int iCnt, iFact = 1;
+    if (iNo<0)
+    {
+        iNo = -iNo;
+    }
+    for (iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+         if (iNo<0)
+        {
+            iNo = -iNo;
+        }
+        if(iCnt % 2 == 0)
+        iFact = iFact * iCnt;
+    }
+    return iFact;
 }
-
-
-
 
 int main()
 {
     int iValue = 0;
-    int iRet =0;
+    int iRet = 0;
 
-    printf("Enter number");
-    scanf("%d", &iValue);
+    printf("enter  number :");
+    scanf("%d",&iValue);
 
-    iRet = DollerToINT(iValue);
+    iRet = EvenFactorial(iValue);
 
-    printf("Value in INR is %d", iRet);
-
+    printf(" Even Factorial of number is %d", iRet);
     return 0;
+
 }
