cfae0d36ee461e06948d5f57011a774505d78445
sakshi kachate <sakshikachate347@gmail.com>
2025-11-09 12:43:09 +0530

    Check Divisibility of 5

---
+++ Assignments/Assignment_12/Program_5.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool is_divisible_by_five(int number )
+{
+    
+    
+  
+        if ( number%5==0) 
+        {
+           return true;
+
+        }
+        else 
+        {
+            return false;
+        }
+        
+}
+   
+
+int main()
+{
+    int number =0;
+    bool bRet = false;
+
+    printf("Enter number ");
+    scanf("%d",&number);
+
+    
+   
+
+    bRet =   is_divisible_by_five(number);
+    if(bRet == true)
+    {
+        printf(" yes is Divisible by 5");
+    }
+    else
+    {
+         printf(" no is not  Divisible by 5");
+    }
+
+
+    return 0;
+}
