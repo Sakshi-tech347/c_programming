c3305c615fc5a30392249906bad41a2d7aefc040
sakshi kachate <sakshikachate347@gmail.com>
2025-11-09 11:32:21 +0530

    Accept number print its greater smaller or large

---
+++ Assignments/Assignment_7/Program_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Number(int iNo)
+{
+
+ if(iNo <50 )
+ printf("small");
+ else if (iNo > 50 && iNo <100)
+ printf("Medium");
+ else 
+ printf("Large");
+
+} // time complexity : N
+
+int main()
+{
+    int iValue = 0;
+
+    printf("enter number:");
+    scanf("%d",&iValue);
+
+    Number(iValue);
+    return 0;
+
+}
