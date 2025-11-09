9115580c79d228f3aac1841a7e4a5a4d5717056e
sakshi kachate <sakshikachate347@gmail.com>
2025-11-09 12:42:07 +0530

    Sum of even Factor (exclude N)

---
+++ Assignments/Assignment_12/Program_4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int sum_of_even_factor(int number )
+{
+    int i ,iSum =0;
+    for(i=1; i< number ; i++)
+    {
+        if (number%i ==0  && i%2 ==0) 
+        {
+            iSum = i+iSum;
+
+        }
+        
+    }
+    return iSum;
+}
+int main()
+{
+    int number ;
+
+    printf("Enter number ");
+    scanf("%d",&number);
+
+   
+
+    printf("%d\n",sum_of_even_factor(number));
+
+
+    return 0;
+}
