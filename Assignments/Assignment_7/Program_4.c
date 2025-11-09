cd3ea0c320e7bcbc0c265b8fc8978c82c35ec1e4
sakshi kachate <sakshikachate347@gmail.com>
2025-11-09 11:43:05 +0530

    print table of given number

---
+++ Assignments/Assignment_7/Program_4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+
+void Table(int iNo) 
+{
+    int iCnt, iFact = 1;
+    if (iNo<0)
+    {
+        iNo = -iNo;
+    }
+    for (iCnt = 1; iCnt <= 10; iCnt++) 
+    {
+       printf("%d\n",iCnt * iNo);
+    }
+   
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("enter  number :");
+    scanf("%d",&iValue);
+
+    Table(iValue);
+
+  
+    return 0;
+
+}
