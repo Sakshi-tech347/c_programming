b507923ae59fa2a98e148825b56d8303534a6fae
sakshi kachate <sakshikachate347@gmail.com>
2025-11-09 12:37:28 +0530

    Display all number reverse in that range

---
+++ Assignments/Assignment_11/Program_5.c
@@ -0,0 +1,39 @@
+
+
+#include <stdio.h>
+
+void RangeDisplay(int iValue1, int iValue2)
+{       int i =0;
+
+    if(iValue1 > iValue2)
+    {
+        printf("invalid input");
+    }
+
+
+      for (i = iValue2; i >= iValue1; i--) 
+      {
+        printf("%d ", i);
+      }
+
+      
+}
+
+int main()
+{
+    int iStart, iEnd, i;
+
+    printf("Enter start number: ");
+    scanf("%d", &iStart);
+
+    printf("Enter end number: ");
+    scanf("%d", &iEnd);
+
+    RangeDisplay(iStart,iEnd);
+
+    //printf("Numbers from %d to %d are:\n", iStart, iEnd);
+
+  
+
+    return 0;
+}
