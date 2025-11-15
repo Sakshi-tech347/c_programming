4f5f222c90023c6ec9508771cbad823c8503060d
sakshi kachate <sakshikachate347@gmail.com>
2025-11-15 19:46:03 +0530

    Add files via upload

---
+++ Class_Work/program32.c
@@ -0,0 +1,26 @@
+
+// Static loop
+
+#include <stdio.h>
+
+void Display()
+{
+  
+    
+    printf("1\t*\t");   
+    printf("1\t*\t"); 
+    printf("1\t*\t");   
+
+    printf("1\t*\t");   
+
+    printf("1\t*\t");   
+
+}
+
+int main()
+{
+
+    Display();
+
+    return 0;
+}
---
+++ Class_Work/program33.c
@@ -0,0 +1,26 @@
+
+// Static loop
+//  *     *    *    *    *
+
+#include <stdio.h>
+
+void Display()
+{
+    int iCnt =0;
+    for (iCnt = 0; iCnt <= 5 ;iCnt++)
+    {
+         printf("1\t*\t");
+    }
+      
+
+}
+
+int main()
+{
+
+    Display();
+    
+    
+    
+    return 0;
+}
