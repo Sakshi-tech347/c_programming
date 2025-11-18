a9e601bec38b5b229550d612432219f5772c2bd6
sakshi kachate <sakshikachate347@gmail.com>
2025-11-18 23:36:29 +0530

    element divisible by 3&5

---
+++ Assignments/Assignment25/Program_4.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void Display(int Arr [], int iSize)
+{
+    int iCnt =0;
+    printf("this element divisible by 3 & 5\n");
+    for(int iCnt =0 ; iCnt < iSize ; iCnt++)
+    {
+        if((Arr[iCnt]) %3 == 0 && (Arr[iCnt]) %5 == 0)
+        {
+         printf(" %d \n",Arr[iCnt] );
+            
+        }
+       
+        
+    }
+  
+}
+
+int main()
+{
+    int iCnt =0 , iSize =0 ,iRet =0;
+    int * iptr = NULL;
+  
+
+    printf("Enter number of Elements:");
+    scanf("%d", & iSize);
+
+
+
+    iptr = (int *) malloc (iSize * sizeof(int));
+    if ( iptr == NULL)
+
+    {
+        printf ("unable to allocate the memory");
+        return -1;
+    }
+    
+    printf("Enter the Elements\n");
+
+    for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+    {
+        scanf("%d",&iptr[iCnt]);
+
+    }
+
+   Display(iptr , iSize);
+
+   
+
+    free(iptr) ;
+
+
+
+    return 0;
+}
