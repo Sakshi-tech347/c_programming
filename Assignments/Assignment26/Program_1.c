243232d11daebba68b098f37379e5c07079c0bdd
sakshi kachate <sakshikachate347@gmail.com>
2025-11-22 00:28:27 +0530

    A B C D E

---
+++ Assignments/Assignment26/Program_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    char ch = 'A';
+
+    for(iCnt =1 ;iCnt <= iNo ; iCnt++)
+    {
+        printf("%c\t",ch);
+        ch++;
+    }
+}
+
+
+
+int main()
+{
+    int iValue = 0;
+    printf("enter number of element:");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+    return 0;
+}
