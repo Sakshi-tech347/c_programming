e285cf2b188c8f8209271b28d0b91137fb213673
sakshi kachate <sakshikachate347@gmail.com>
2025-11-22 23:48:52 +0530

    1 2 3 in 3 row

---
+++ Assignments/Assignment27/program_2.c
@@ -0,0 +1,44 @@
+/*
+ ip : row 4 
+      col 3
+ 
+                 i          
+ op :1 2 3  j  1,1  1,2  1,3  1,4
+     1 2 3     2,1  2,2  2,3  2,4
+     1 2 3     3,1  3,2  3,3  3,4
+     1 2 3     4,1  4,2  4,3  4,4
+
+*/
+
+
+#include<stdio.h>
+
+void pattern(int iRow, int iCol)
+{
+    int i=0, j=0;
+
+    for(i=1 ; i<=iRow ; i++)
+    {
+        for(j=1; j <=iCol ; j++)
+        {
+            printf("%d\t",j);
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 =0, iValue2 =0;
+    
+    printf("enter the number of row:");
+    scanf("%d",&iValue1);
+
+    printf("enter the number of column:");
+    scanf("%d",&iValue2);
+    
+    pattern(iValue1,iValue2);
+    
+    return 0;
+}
