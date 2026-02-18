5d0256a10d7bf862ca92abf666e0af02fde7ada5
sakshi kachate <sakshikachate347@gmail.com>
2026-02-18 12:56:10 +0530

    recursion

---
+++ Assignments/Assignment69/program1_1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+     static int i = 1;
+     if ( i<= iNo )
+     {
+        cout<<"*\t";
+        i++ ;
+        Display(iNo);
+     }
+}
+
+int main()
+{
+    int iValue = 0;
+    cout<<"Enter Number :\n";
+
+    cin>>iValue;
+    Display(iValue);
+
+    return 0;
+}
